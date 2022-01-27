323c79cb8108e23413cc557c80a33214eae69a7a
unknown <vqh.official@gmail.com>
2022-01-27 17:54:01 +0700

    config firebase

--- frontend/App.js
+++ frontend/App.js
@@ -2,7 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import LandingScreen from './components/auth/Landing';
-import {createStackNavigator} from '@react-navigation/stack'
+import {createStackNavigator} from '@react-navigation/stack';
+import  firebase from 'firebase';
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC3w31bxsHYBM7gY92AUkJYRZd3QvLZg5Y",
+  authDomain: "instagram-dev-6ecec.firebaseapp.com",
+  projectId: "instagram-dev-6ecec",
+  storageBucket: "instagram-dev-6ecec.appspot.com",
+  messagingSenderId: "320624387519",
+  appId: "1:320624387519:web:b6a2b7de347b56a2a02d0a",
+  measurementId: "G-S0MYHBHP07"
+};
+
+if(firebase.apps.length===0){
+  firebase.initializeApp(firebaseConfig);
+}
 
 const Stack = createStackNavigator();
 export default function App() {
